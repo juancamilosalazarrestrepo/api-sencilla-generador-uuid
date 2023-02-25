fe82a2ff37c3ee509ca94b9a3bb2436275190422
juancamilosalazarrestrepo <juancamilosalazarrestrepo@gmail.com>
2023-02-24 19:10:48 -0500

    enable ssl

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const fs = require('fs');
 const http = require("http");
+const https = require("https");
 const helmet = require("helmet");
 let compression = require("compression");
 require("dotenv").config();
 const { v4: uuidv4 } = require("uuid");
 const cors = require("cors");
+
+const httpsServerOptions = {
+    key : fs.readFileSync(process.env.KEY_PATH),
+    cert : fs.readFileSync(process.env.CERT_PATH)
+};
 
 const app = express();
 app.use(helmet({ contentSecurityPolicy: false }));
@@ -13,6 +20,13 @@ app.use(cors());
 
 const serverHttp = http.createServer(app);
 serverHttp.listen(process.env.HTTP_PORT, process.env.IP);
+
+const serverHttps = https.createServer(httpsServerOptions,app);
+serverHttps.listen(process.env.HTTPS_PORT, process.env.IP);
+
+app.use((req,res,next)=> {
+    if (req.secure) next(); else res.redirect(`https://${req.headers.host}${req.url}`);
+});
 
 app.use(express.static("./public"));
 
